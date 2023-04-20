f24131e72e3ee1a9e5c7207061b20aa213b2405a
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-04-20 09:27:16 -0400

    Initial Commit

---
+++ CarTracker/CarTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTrackerApp.swift
+//  CarTracker
+//
+//  Created by Sebastian Frosch on 20.04.23.
+//
+
+import SwiftUI
+
+@main
+struct CarTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarTracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CarTracker
+//
+//  Created by Sebastian Frosch on 20.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
